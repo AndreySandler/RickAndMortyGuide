2fd0062dfe5193f29c5bc23e607e78325f7c175b
Andrey Sandler <mrdreem13@gmail.com>
2024-07-18 17:21:44 +0300

    Initial Commit

---
+++ RickAndMortyGuide/RickAndMortyGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyGuideApp.swift
+//  RickAndMortyGuide
+//
+//  Created by Andrey Sandler on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
